b43abafc6cc8e363c4179acece7bd85aafaf4238
rapraprafa <rafaeljohnbalayo@gmail.com>
2020-03-20 20:33:25 +0800

    second commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ServersComponent } from '../servers/servers.component';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent{
+    serverId = 10;
+    serverStatus = "offline";
+
+    @Input() serverName;
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+
+    getColor = () => {return this.serverStatus === 'online' ? 'green' : 'red';}
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-servers',
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "Servertest";
+  backgroundColor = "";
+  serverCreated = false;
+  servers = ["Test server", "Test server 2"];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  test = () => console.log("ez");
+
+  changeButtonColor() {
+    if (this.backgroundColor != "background-color:black;") {
+      this.backgroundColor = "background-color:black;";
+    }
+    else{
+      this.backgroundColor = "";
+    }
+  }
 }
